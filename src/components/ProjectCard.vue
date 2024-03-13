7ee4ac846cd62448a3bcc452779a55bf00616800
Drew0004 <drdrewgraphic@gmail.com>
2024-03-13 16:09:20 +0100

    Done Milestone 5 + bonus

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    data() {
+        return {
+            basePath: 'http://127.0.0.1:8000/storage/'
+        };
+    },
+    methods: {
+
+    },
+    props:{
+        singleProject: Object,
+    }
+}
+</script>
+
+<template>
+        <div class="col-auto">
+            <div class="card" style="width: 18rem; min-height: 600px;">
+                <div class="img-container">
+                    <img :src="basePath+singleProject.cover_img" class="card-img-top w-100 object-fit-contain" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ singleProject.title }}</h5>
+                    
+                    <p class="card-text">{{ singleProject.description }}</p>
+                    <div class="py-1" v-for="singleTechnology in singleProject.technologies">
+                        <span class="badge text-bg-primary">{{ singleTechnology.title }}</span>
+                    </div>
+                    <div class="py-1">
+                        <span class="badge text-bg-success">{{ singleProject.type.title }}</span>
+                    </div>
+                    
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+</template>
+
+<style lang="scss" scoped>
+    .img-container{
+        width: 100%;
+        height: 300px;
+    }
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,28 +1,63 @@
 <script>
 import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
 export default {
     data() {
         return {
-            projects: [], 
+            projects: [],
+            currentPage: 1,
+            lastPage: 3, 
         };
     },
-    methods: {
+    components:{
+        ProjectCard
     },
-    mounted(){
-        axios.get('http://127.0.0.1:8000/api/projects')
+    created(){
+        this.getDataFromApi(this.currentPage);
+    },
+    methods: {
+        getDataFromApi(page){
+            axios.get('http://127.0.0.1:8000/api/projects',
+            {
+                params:{
+                page : page
+                }
+            })
             .then((response)=>{
-                console.log(response.data.results.data);
                 this.projects = response.data.results.data;
 
-            });
-        console.log('Progetto:'+this.projects);
-    }
+            }); 
+        },
+        
+        goToPrevPage(){
+            if (this.currentPage > 1) {
+                this.getDataFromApi(this.currentPage -1)
+                console.log('prev')
+            }
+        },
+
+        goToNextPage(){
+            if (this.currentPage < this.lastPage){
+                this.getDataFromApi(this.currentPage +1)
+                console.log('next')
+            }
+        }
+    },
 }
 </script>
 
 <template>
     <main>
-        <h1>Home page</h1>
+        <h1 class="text-center py-5">Home page</h1>
+        <div class="container">
+            <div class="row">
+                <ProjectCard v-for="(singleProject, i) in projects" :key="i" :singleProject="singleProject"/>
+                <div class="my-3">
+                    <button @click="goToPrevPage()" class="col-1 btn btn-success me-3 ">Previous</button>
+                    <button @click="goToNextPage()" class="col-1 btn btn-primary ">Next</button>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
