552635f1f92aed0d8ae271e3861d3655d76ac917
Andrea Aliquò <146461081+Drew0004@users.noreply.github.com>
2024-03-13 14:31:19 +0100

    Initial commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <main>
+        <h1>Home page</h1>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+</style>
